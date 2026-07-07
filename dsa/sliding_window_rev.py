f41d51d661b775267db5bd13d531db7316271c2b
Your Name <manojilango97@gmail.com>
2026-07-07 00:37:12 -0400

    Sliding Window revision - fixed window skeleton rebuilt from scratch

---
+++ dsa/sliding_window_rev.py
@@ -0,0 +1,15 @@
+def max_sum_window(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+
+    for i in range(k, len(nums)):
+        window_sum += nums[i]
+        window_sum -= nums[i - k]
+        max_sum = max(max_sum, window_sum)
+
+    return max_sum
+
+
+nums = [1, 2, 3, 4, 5, 6]
+k = 3
+print(max_sum_window(nums, k))
