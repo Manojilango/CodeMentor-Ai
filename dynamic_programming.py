e31068948fae1d78ed7a11fa2bdb216e935eb9ba
Your Name <manojilango97@gmail.com>
2026-07-18 14:02:29 -0400

    DP revision - Climbing Stairs rebuilt, bottom-up approach confirmed

---
+++ dynamic_programming.py
@@ -0,0 +1,14 @@
+def climbStairs(n):
+    if n <= 2:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+
+print(climbStairs(5))   # expected: 8
+print(climbStairs(4))   # expected: 5
+print(climbStairs(1))   # expected: 1
