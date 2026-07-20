e8d7416587f0dcb7cd823b2cf1016fc37a9f627c
Your Name <manojilango97@gmail.com>
2026-07-20 16:49:52 -0400

    LeetCode 300 - Longest Increasing Subsequence done

---
+++ lengtghoflts.py
@@ -0,0 +1,13 @@
+def lengthOfLIS(nums):
+    dp = [1] * len(nums)
+    
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    
+    return max(dp)
+
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+print(lengthOfLIS(nums))   # expected: 4
